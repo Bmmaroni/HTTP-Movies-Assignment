d340e9ad58fa8b6829c906ede10ad88bcc8a0e09
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-19 21:43:19 -0700

    updates to AddMovie to attempt to get proper functionality; still working

--- client/src/Movies/AddMovie.js
+++ client/src/Movies/AddMovie.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useParams, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const initialState = {
+  id: '',
   title: '',
   director: '',
   metascore: '',
@@ -11,17 +12,66 @@ const initialState = {
 
 const AddMovie = (props) => {
   const [formState, setFormState] = useState(initialState);
+  const { push } = useHistory();
+
+  const handleChanges = e => {
+    e.persist();
+    setFormState({...formState, [e.target.name]: e.target.value});
+  };
+
+  const submitForm = e => {
+    e.preventDefault();
+    const updatedMovie = {
+      ...formState,
+      stars: formState.stars.split(',')
+    }
+    axios
+      .post(`http://localhost:5000/api/movies`, updatedMovie)
+      .then(res => {
+        console.log('Adding Movie:', res.data);
+        props.setMovieList(
+          ...props.movieList,
+          res.data
+        );
+        setFormState(initialState);
+        push('/');
+      })
+      .catch(err => console.log(err));
+  }
 
   return (
-    <div>
+    <div className='save-wrapper'>
       <h2>Add A Movie</h2>
-      <form>
+      <form onSubmit={submitForm}>
         <input
           type='text'
           name='title'
+          placeholder='Title'
           value={formState.title}
           onChange={handleChanges}
         />
+        <input
+          type='text'
+          name='director'
+          placeholder='Director'
+          value={formState.director}
+          onChange={handleChanges}
+        />
+        <input
+          type='text'
+          name='metascore'
+          placeholder='Metascore'
+          value={formState.metascore}
+          onChange={handleChanges}
+        />
+        <input
+          type='text'
+          name='stars'
+          placeholder='Stars'
+          value={formState.stars}
+          onChange={handleChanges}
+        />
+        <button>Add</button>
       </form>
     </div>
   )
